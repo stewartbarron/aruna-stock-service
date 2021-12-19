88660d4f134c53525b7d8b1077ab5cc0679e8b23
Stewart Barron <stewartbarron@Stewarts-iMac.local>
2021-12-19 20:37:06 +0000

    initial commit

---
+++ src/test/java/com/github/stewartbarron/aruna/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stewartbarron.aruna.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
